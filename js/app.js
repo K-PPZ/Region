a963409fc8d5a09afdae76ff7e90dea061f0d4fb
KorenPapazian <kpapazian@edenschool.fr>
2022-06-03 14:57:25 +0200

    HTML and JS

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const ARA = document.getElementById("ARA");
+const bretagne = document.getElementById("bretagne");
+const normandie = document.getElementById("normandie");
+
+ARA.addEventListener("click", () => {
+    
+});
+
+bretagne.addEventListener("click", () => {
+    
+});
+
+normandie.addEventListener("click", () => {
+    
+});
